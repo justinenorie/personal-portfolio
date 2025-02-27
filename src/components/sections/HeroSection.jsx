86c9bbb340bd724f29bdbac07d6a0bc67c31b12d
justinenorie <justinedelacruz97@gmail.com>
2025-02-27 22:24:33 +0800

    Hero Section layout

--- src/components/sections/HeroSection.jsx
+++ src/components/sections/HeroSection.jsx
@@ -1,28 +1,37 @@
 import Typography from '../ui/Typography';
 
-function HeroSection() {
+const HeroSection = () => {
   return (
-    <section>
-      <Typography variant="h1" className="text-txt-light">
-        HeroSection
-      </Typography>
-      <h2>Hello, I am</h2>
-      <h1>Justine Norie</h1>
-      <h3>Front-End Developer</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis,
-        velit vitae interdum aliquet, risus metus bibendum ligula, ut placerat
-        metus purus non odio. Lorem ipsum dolor sit amet, consectetur adipiscing
-        elit.{' '}
-      </p>
-      {/* <Downloadbtn
-        btn={"Download my CV"}
-        className={
-          "bg-primary hover:bg-secondary text-text font-bold py-2 px-4 rounded-full"
-        }
-      /> */}
+    <section className="flex flex-col-reverse md:grid md:grid-cols-2 mt-8">
+      <div className="content-center">
+        <Typography variant="h3" className="text-txt-light py-2">
+          Hello, I'm
+        </Typography>
+
+        <Typography variant="h1" className="text-txt-light py-2">
+          Justine Norie
+        </Typography>
+
+        <Typography variant="h3" className="text-transparent bg-clip-text bg-gradient-to-r from-primary from-20% via-accent via-45% to-secondary to-90%">
+          Full Stack Developer
+        </Typography>
+
+        <Typography variant="h3" className="text-transparent bg-clip-text bg-gradient-to-r from-primary from-0% via-accent via-35% to-secondary to-75%">
+          UI/UX Designer
+        </Typography>
+
+        <Typography variant="p" className="text-txt-light py-2">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mollis,
+          velit vitae interdum aliquet, risus metus bibendum ligula, ut placerat
+          metus purus non odio. Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit.
+        </Typography>
+      </div>
+      <div className="flex md:justify-end justify-center md:items-end items-center">
+        <img src="/Rectangle.svg" alt="sample-img" />
+      </div>
     </section>
   );
-}
+};
 
 export default HeroSection;
