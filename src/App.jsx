6dda3ae1249d4b2cec058c7204ad015ec188c073
justinenorie <justinedelacruz97@gmail.com>
2025-03-01 20:04:46 +0800

    Added framer-motion and motion libraries; implemented animated background effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useRef, useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import HeroSection from './components/sections/HeroSection';
 import TopBar from '@/components/sections/TopBar';
 import Skills from './components/sections/Skills';
@@ -6,6 +8,23 @@ import Experience from './components/sections/Experience';
 import Footer from './components/sections/Footer';
 
 function App() {
+  const ref = useRef(null);
+  const [coordinates, setCoordinates] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    if (ref.current) {
+      const handlePointerMove = ({ clientX, clientY }) => {
+        const element = ref.current;
+        const x = clientX - element.offsetLeft - element.offsetWidth / 2;
+        const y = clientY - element.offsetTop - element.offsetHeight / 2;
+        setCoordinates({ x, y });
+      };
+
+      window.addEventListener('pointermove', handlePointerMove);
+      return () => window.removeEventListener('pointermove', handlePointerMove);
+    }
+  }, []);
+
   return (
     <div>
       <TopBar />
@@ -14,6 +33,14 @@ function App() {
       <Projects />
       <Experience />
       <Footer />
+      <motion.div
+        ref={ref}
+        className="fixed h-30 w-30 blur-3xl bg-accent rounded-full shadow-lg -z-10"
+        animate={{ x: coordinates.x, y: coordinates.y }}
+        transition={{
+          type: 'spring',
+        }}
+      />
     </div>
   );
 }
